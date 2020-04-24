ee391630756ae45a12ef098245a32c62332769f4
Amartey Armar <59967301+Spora-GH@users.noreply.github.com>
2020-04-24 08:11:59 +0000

    Update src/lib.rs return types to 'HandlerResult'
    ... to reflect changes in 0.7.1

--- src/lib.rs
+++ src/lib.rs
@@ -19,11 +19,11 @@ use actor::prelude::*;
 actor_handlers! { codec::http::OP_HANDLE_REQUEST => hello_world, 
                   codec::core::OP_HEALTH_REQUEST => health }
 
-fn hello_world(_payload: codec::http::Request) -> ReceiveResult {
+fn hello_world(_payload: codec::http::Request) -> HandlerResult<codec::http::Response> {
     Ok(serialize(codec::http::Response::ok())?)
 }
 
 fn health(_req: codec::core::HealthRequest
-) -> ReceiveResult {
-    Ok(vec![])
+) -> HandlerResult<()> {
+    Ok(())
 }
