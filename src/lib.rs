f536a55d397d829974c9450b242d42a5539b1d2e
Amartey Armar <59967301+Spora-GH@users.noreply.github.com>
2020-04-24 09:06:36 +0000

    removed serialization of hello_world response
    to return a codec::http::Response type

--- src/lib.rs
+++ src/lib.rs
@@ -20,7 +20,7 @@ actor_handlers! { codec::http::OP_HANDLE_REQUEST => hello_world,
                   codec::core::OP_HEALTH_REQUEST => health }
 
 fn hello_world(_payload: codec::http::Request) -> HandlerResult<codec::http::Response> {
-    Ok(serialize(codec::http::Response::ok())?)
+    Ok(codec::http::Response::ok())
 }
 
 fn health(_req: codec::core::HealthRequest
