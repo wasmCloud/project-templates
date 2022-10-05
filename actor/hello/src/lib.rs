c029217eab471c70236a542f4473699041e88047
Brooks Townsend <brooks@cosmonic.com>
2022-10-05 09:48:31 -0400

    trimmed a few unneeded things from the hello sample
    Signed-off-by: Brooks Townsend <brooks@cosmonic.com>

--- actor/hello/src/lib.rs
+++ actor/hello/src/lib.rs
@@ -1,5 +1,5 @@
 use wasmbus_rpc::actor::prelude::*;
-use wasmcloud_interface_httpserver::{HttpRequest, HttpResponse, HttpServer, HttpServerReceiver};
+use wasmcloud_interface_httpserver::{HttpRequest, HttpResponse};
 
 #[derive(Debug, Default, Actor, HealthResponder)]
 #[services(Actor, HttpServer)]
@@ -8,15 +8,10 @@ struct {{to_pascal_case project-name}}Actor {}
 /// Implementation of HttpServer trait methods
 #[async_trait]
 impl HttpServer for {{to_pascal_case project-name}}Actor {
-
     /// Returns a greeting, "Hello World", in the response body.
     /// If the request contains a query parameter 'name=NAME', the
     /// response is changed to "Hello NAME"
-    async fn handle_request(
-        &self,
-        _ctx: &Context,
-        req: &HttpRequest,
-    ) -> std::result::Result<HttpResponse, RpcError> {
+    async fn handle_request(&self, _ctx: &Context, req: &HttpRequest) -> RpcResult<HttpResponse> {
         let text = form_urlencoded::parse(req.query_string.as_bytes())
             .find(|(n, _)| n == "name")
             .map(|(_, v)| v.to_string())
