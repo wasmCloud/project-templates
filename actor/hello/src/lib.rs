25a644402d002665aefe8ce6433d488996063f40
Brooks Townsend <brooksmtownsend@gmail.com>
2023-04-24 13:06:46 -0400

    Remove makefiles, update hello, add kvcounter (#65)
    * improved and simplified hello template
    
    Signed-off-by: Brooks Townsend <brooks@cosmonic.com>
    
    * removed echo and makefiles
    
    Signed-off-by: Brooks Townsend <brooks@cosmonic.com>
    
    * added kvcounter template
    
    Signed-off-by: Brooks Townsend <brooks@cosmonic.com>
    
    ---------
    
    Signed-off-by: Brooks Townsend <brooks@cosmonic.com>

--- actor/hello/src/lib.rs
+++ actor/hello/src/lib.rs
@@ -5,22 +5,11 @@ use wasmcloud_interface_httpserver::{HttpRequest, HttpResponse, HttpServer, Http
 #[services(Actor, HttpServer)]
 struct {{to_pascal_case project-name}}Actor {}
 
-/// Implementation of HttpServer trait methods
+/// Implementation of the HTTP server capability
 #[async_trait]
 impl HttpServer for {{to_pascal_case project-name}}Actor {
-    /// Returns a greeting, "Hello World", in the response body.
-    /// If the request contains a query parameter 'name=NAME', the
-    /// response is changed to "Hello NAME"
-    async fn handle_request(&self, _ctx: &Context, req: &HttpRequest) -> RpcResult<HttpResponse> {
-        let text = form_urlencoded::parse(req.query_string.as_bytes())
-            .find(|(n, _)| n == "name")
-            .map(|(_, v)| v.to_string())
-            .unwrap_or_else(|| "World".to_string());
-
-        Ok(HttpResponse {
-            body: format!("Hello {}", text).as_bytes().to_vec(),
-            ..Default::default()
-        })
+    async fn handle_request(&self, _ctx: &Context, _req: &HttpRequest) -> RpcResult<HttpResponse> {
+        HttpResponse::ok("Hello, World!")
     }
 }
 
